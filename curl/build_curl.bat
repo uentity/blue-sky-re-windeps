68739e6d715bf51085bab13aeda23301d82efc0a
iscancer <iscancer@yandex.ru>
2021-05-06 18:44:05 +0500

    curl: updated to v7.75.0

--- curl/build_curl.bat
+++ curl/build_curl.bat
@@ -9,55 +9,35 @@ set log_path="%~dp0\build.txt"
 date /T > %log_path%
 time /T >> %log_path%
 
-set vcvars_path="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
-call %vcvars_path% x64 >> %log_path% 2>&1
+set vcvars_path="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat"
+call %vcvars_path% x86_x64 >> %log_path% 2>&1
+
+timeout /t 3 /nobreak
 
 set openssl_path=%1
 set CC=cl.exe /DUSE_OPENSSL_NTLM
 
-rem echo "Building x86_64 Release..."
-rem nmake /f Makefile.vc mode=dll WITH_SSL=dll ENABLE_SSPI=yes MACHINE=x64 WITH_DEVEL=%openssl_path%\x86_64_release >> %log_path% 2>&1
-rem if errorlevel 1 goto fail
-rem set out_dir=%~dp0\..\builds\libcurl-vc-x64-release-dll-ssl-dll-ipv6-sspi
-rem set target_dir=%BLUE_SKY_WINDEPS%\curl
-rem del /q %target_dir%\curl\* >> %log_path% 2>&1
-rem xcopy /e /Y %out_dir%\include\curl %target_dir%\curl >> %log_path% 2>&1
-rem del /q %target_dir%\lib64\* >> %log_path% 2>&1
-rem xcopy %out_dir%\lib\libcurl.lib %target_dir%\lib64\
-rem xcopy %out_dir%\bin\libcurl.dll %target_dir%\lib64\
-rem echo "Success"
+echo "Building x86_64 Release..."
+nmake /f Makefile.vc mode=dll WITH_SSL=dll ENABLE_SSPI=yes MACHINE=x64 WITH_DEVEL=%openssl_path%\x86_64_release >> %log_path% 2>&1
+if errorlevel 1 goto fail
+set out_dir=%~dp0\..\builds\libcurl-vc-x64-release-dll-ssl-dll-ipv6-sspi
+set target_dir=%BLUE_SKY_WINDEPS%\curl
+del /q %target_dir%\include\* >> %log_path% 2>&1
+xcopy /e /Y %out_dir%\include %target_dir%\include >> %log_path% 2>&1
+del /q %target_dir%\lib\* >> %log_path% 2>&1
+xcopy %out_dir%\lib\libcurl.lib %target_dir%\lib\
+xcopy %out_dir%\bin\libcurl.dll %target_dir%\lib\
+echo "Success"
 
 echo "Building x86_64 Debug..."
 nmake /f Makefile.vc mode=dll WITH_SSL=dll ENABLE_SSPI=yes MACHINE=x64 DEBUG=yes WITH_DEVEL=%openssl_path%\x86_64_debug >> %log_path% 2>&1
 if errorlevel 1 goto fail
 set out_dir=%~dp0\..\builds\libcurl-vc-x64-debug-dll-ssl-dll-ipv6-sspi
-set target_dir=%BLUE_SKY_WINDEPS%\curl\debug\lib64
+set target_dir=%BLUE_SKY_WINDEPS%\curl\lib\debug
 del /q %target_dir%\* >> %log_path% 2>&1
-copy /Y %out_dir%\lib\libcurl.lib %target_dir%\libcurl.lib
-copy /Y %out_dir%\bin\libcurl.dll %target_dir%\libcurl.dll
+copy /Y %out_dir%\lib\libcurl_debug.lib %target_dir%\libcurl.lib
+copy /Y %out_dir%\bin\libcurl_debug.dll %target_dir%\libcurl.dll
 echo "Success"
-
-rem call %vcvars_path% x86 >> %log_path% 2>&1
-
-rem echo "Building x86 Release..."
-rem nmake /f Makefile.vc mode=dll WITH_SSL=dll ENABLE_SSPI=yes MACHINE=x86 WITH_DEVEL=%openssl_path%\x86_release >> %log_path% 2>&1
-rem if errorlevel 1 goto fail
-rem set out_dir=%~dp0\..\builds\libcurl-vc-x86-release-dll-ssl-dll-ipv6-sspi
-rem set target_dir=%BLUE_SKY_WINDEPS%\curl\lib
-rem del /q %target_dir%\* >> %log_path% 2>&1
-rem xcopy %out_dir%\lib\libcurl.lib %target_dir%\
-rem xcopy %out_dir%\bin\libcurl.dll %target_dir%\
-rem echo "Success"
-
-rem echo "Building x86 Debug..."
-rem nmake /f Makefile.vc mode=dll WITH_SSL=dll ENABLE_SSPI=yes MACHINE=x86 DEBUG=yes WITH_DEVEL=%openssl_path%\x86_debug >> %log_path% 2>&1
-rem if errorlevel 1 goto fail
-rem set out_dir=%~dp0\..\builds\libcurl-vc-x86-debug-dll-ssl-dll-ipv6-sspi
-rem set target_dir=%BLUE_SKY_WINDEPS%\curl\debug\lib
-rem del /q %target_dir%\* >> %log_path% 2>&1
-rem copy /Y %out_dir%\lib\libcurl.lib %target_dir%\libcurl.lib
-rem copy /Y %out_dir%\bin\libcurl.dll %target_dir%\libcurl.dll
-rem echo "Success"
 
 exit /B
 
